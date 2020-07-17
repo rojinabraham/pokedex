3056ba049c085aa1b75f0dfec08abb168bb19ac4
Rojin Abraham <rojinabraham@gmail.com>
2020-07-17 16:55:18 +0530

    Updates

---
+++ precache-manifest.f43a8f1e8599349b03642589e64ae0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe48b75f42bfbf6ca6d50af00218e61a",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "68251e3b2e77bccf7695",
+    "url": "/pokemon/static/css/main.903eece9.chunk.css"
+  },
+  {
+    "revision": "f6c256ca51b9c056827b",
+    "url": "/pokemon/static/js/2.0b85763f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.0b85763f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68251e3b2e77bccf7695",
+    "url": "/pokemon/static/js/main.99e15c6a.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  }
+]);
